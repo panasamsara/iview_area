f9b3126bd129d51088f84741e39cbf7d6ce77447
joshua <panasamsara@126.com>
2018-11-19 15:05:51 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# ivew_area
-ivew Cascader 级联选择组件 省市区 三级联动
+# iview_area
+
+> iview Cascader 级联选择组件 省市区 三级联动
+
+## 安装后查看效果
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+```
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,48 @@
+<template>
+  <div style="padding: 15px;">
+    <Row>
+      <Col span="4">
+      <Cascader :data="data2" v-model="value2" @on-change="getArea"></Cascader>
+      </Col>
+      <Col span="4" style="text-align: center">
+      <Button type="info" @click="query">查询</Button>
+      <Button type="info" @click="reset">重置</Button>
+      </Col>
+    </Row>
+  </div>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        value2: ['11', '1101', '110102'],
+        data2: this.AREA,
+        args: ['北京', '市辖区', '西城区']
+      }
+    },
+    created() {
+
+    },
+    methods: {
+      query() {
+        console.log(this.args)
+      },
+      reset() {
+        this.value2 = []
+        this.args = []
+      },
+      getArea(value, selectedData) {
+        const areaName = selectedData.map(item => item.label)
+        this.args = [...areaName]
+      }
+    }
+  }
+
+</script>
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+
+</style>
